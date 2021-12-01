1a45390d3354a4d155eb83b596c4adf761ab788c
Rasha Moumneh <rasha@fastmailteam.com>
2021-12-01 16:08:50 -0500

    day 01, puzzle 02

---
+++ day01/puzzle02.pl
@@ -0,0 +1,62 @@
+use strict;
+use warnings;
+use Path::Tiny;
+
+my @input = path('puzzle01-input')->lines;
+
+my $increase_count = 0;
+my $current_sum = 0;
+my $sum;        
+    
+for(my $i = 0; $i <= $#input; $i++) {
+  if ($input[$i] && $input[$i+1] && $input[$i+2]) {
+    $sum = $input[$i] + $input[$i+1] + $input[$i+2]; 
+    if ($sum > $current_sum && $i > 0) {
+      $increase_count++;
+    };
+    $current_sum = $sum;
+  };
+};
+
+print $increase_count;
+
+
+
+=pod
+
+Considering every single measurement isn't as useful as you expected: there's just too much noise in the data.
+
+Instead, consider sums of a three-measurement sliding window. Again considering the above example:
+
+199  A      
+200  A B    
+208  A B C  
+210    B C D
+200  E   C D
+207  E F   D
+240  E F G  
+269    F G H
+260      G H
+263        H
+
+Start by comparing the first and second three-measurement windows. The measurements in the first window are marked A (199, 200, 208); their sum is 199 + 200 + 208 = 607. The second window is marked B (200, 208, 210); its sum is 618. The sum of measurements in the second window is larger than the sum of the first, so this first comparison increased.
+
+Your goal now is to count the number of times the sum of measurements in this sliding window increases from the previous sum. So, compare A with B, then compare B with C, then C with D, and so on. Stop when there aren't enough measurements left to create a new three-measurement sum.
+
+In the above example, the sum of each three-measurement window is as follows:
+
+A: 607 (N/A - no previous sum)
+B: 618 (increased)
+C: 618 (no change)
+D: 617 (decreased)
+E: 647 (increased)
+F: 716 (increased)
+G: 769 (increased)
+H: 792 (increased)
+
+In this example, there are 5 sums that are larger than the previous sum.
+
+Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
+
+=cut
+
